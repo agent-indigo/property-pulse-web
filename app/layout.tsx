8fee0e8467668afe892de9e2dcdbb6cfc535e202
agent-indigo <bradenhynes@gmail.com>
2024-05-25 17:34:52 -0230

    Added a type assertion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import {ReactNode} from 'react'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import {NextFont} from 'next/dist/compiled/@next/font'
 import AuthProvider from '@/components/AuthProvider'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import '@/assets/styles.css'
-const inter = Inter({subsets: ['latin']})
+const inter: NextFont = Inter({subsets: ['latin']})
 export const metadata: Metadata = {
   title: {
     template: '%s | PropertyPulse | Find the Perfect Rental',
