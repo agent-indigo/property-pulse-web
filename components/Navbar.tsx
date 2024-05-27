2d2c46311fe78d2b5396da4d3518428e60e71449
agent-indigo <bradenhynes@gmail.com>
2024-05-27 11:10:29 -0230

    Code formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -100,11 +100,15 @@ const Navbar: React.FC = () => {
             </div>
           </div>
           {/* user menu (logged out) */}
-          {!session && (
+          {!session && providers && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
                 {providers && Object.values(providers).map((provider: ClientSafeProvider, index: number) => (
-                  <button key={index} onClick={() => signIn(provider.id)} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <button
+                    key={index}
+                    onClick={() => signIn(provider.id)}
+                    className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                  >
                     <FaGoogle className='text-white mr-2'/>
                     <span>
                       Log In or Register
@@ -248,7 +252,11 @@ const Navbar: React.FC = () => {
               </Link>
             )}
             {!session && providers && Object.values(providers).map((provider: ClientSafeProvider, index: number) => (
-              <button key={index} onClick={() => signIn(provider.id)} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+              <button
+                key={index}
+                onClick={() => signIn(provider.id)}
+                className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+              >
                 <FaGoogle className='text-white mr-2'/>
                 <span>
                   Log In or Register
