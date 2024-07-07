a6190de7ec957c215cedd20747252dd146b654fc
agent-indigo <bradenhynes@gmail.com>
2024-07-07 15:55:30 -0230

    Google sign in bug fixed!:D

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**'
+      }
+    ]
+  }
+}
+export default nextConfig
