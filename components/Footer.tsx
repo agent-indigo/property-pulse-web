acaa91e0d541d265a7afe22f846afafbea65c5b4
agent-indigo <bradenhynes@gmail.com>
2024-08-07 17:17:52 -0230

    Cleanup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,23 +2,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {ReactElement} from 'react'
 import logo from '@/assets/images/logo.png'
-const Footer: React.FC = (): ReactElement => {
-  const currentYear: number = new Date().getFullYear()
-  return (
-    <footer className='bg-gray-200 py-4 mt-24'>
-      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
-        <div className='mb-4 md:mb-0'>
-          <Link href='/'>
-            <Image src={logo} alt='Logo' className='h-8 w-auto'/>
-          </Link>
-        </div>
-        <div>
-          <p className='text-sm text-gray-500 mt-2 md:mt-0'>
-            &copy; {currentYear} PropertyPulse. All rights reserved.
-          </p>
-        </div>
+const Footer: React.FC = (): ReactElement =>  (
+  <footer className='bg-gray-200 py-4 mt-24'>
+    <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
+      <div className='mb-4 md:mb-0'>
+        <Link href='/'>
+          <Image src={logo} alt='Logo' className='h-8 w-auto'/>
+        </Link>
       </div>
-    </footer>
-  )
-}
+      <div>
+        <p className='text-sm text-gray-500 mt-2 md:mt-0'>
+          &copy; {new Date().getFullYear()} PropertyPulse. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </footer>
+)
 export default Footer
