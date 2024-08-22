172bfec8e0edddae56ee4a12aac77e804ee66cdd
agent-indigo <bradenhynes@gmail.com>
2024-08-22 20:05:21 -0230

    Bug fix

--- slices/unreadMessagesCountSlice.ts
+++ slices/unreadMessagesCountSlice.ts
@@ -1,7 +1,7 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import {UnreadMessagesCountState} from '@/utilities/interfaces'
 const initialState: UnreadMessagesCountState = {
-  unreadMessagesCount: JSON.parse(localStorage.getItem('unreadMessagesCount') ?? '0')
+  unreadMessagesCount: 0
 }
 const unreadMessagesCountSlice = createSlice({
   name: 'unreadMessagesCount',
@@ -18,12 +18,11 @@ const unreadMessagesCountSlice = createSlice({
       action: PayloadAction<number>
     ): void => {
       state.unreadMessagesCount = action.payload
-      localStorage.setItem(
-        'unreadMessagesCount',
-        JSON.stringify(action.payload)
-      )
     }
   }
 })
-export const {increment, decrement, set} = unreadMessagesCountSlice.actions
+export const {
+  increment,
+  decrement
+} = unreadMessagesCountSlice.actions
 export default unreadMessagesCountSlice.reducer
