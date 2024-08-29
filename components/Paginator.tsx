28b37a5cdb58d5847d8a93a33262a0133f0c2e96
agent-indigo <bradenhynes@gmail.com>
2024-08-29 18:11:09 -0230

    Fixed Paginator bug

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -7,12 +7,14 @@ const Paginator: FunctionComponent<PaginatorProps> = ({
   size = 6,
   fromSearch = false
 }): ReactElement => {
+  const current: number = parseInt(page.toString())
+  const destination: string = fromSearch ? '/properties/search?page=' : '/properties?page='
   const pages: number = Math.ceil(total / size)
   return (
     <section className='container mx-auto flex justify-center items-center my-8'>
       {page > 1 && (
         <Link
-          href={`/properties${fromSearch && '/search'}?page=${page - 1}`}
+          href={`${destination}${current - 1}`}
           className='mr-2 px-2 py-1 border border-gray-300 rounded'
         >
           Previous
@@ -23,7 +25,7 @@ const Paginator: FunctionComponent<PaginatorProps> = ({
       </span>
       {page < pages && (
         <Link
-          href={`/properties${fromSearch && '/search'}?page=${page + 1}`}
+          href={`${destination}${current + 1}`}
           className='ml-2 px-2 py-1 border border-gray-300 rounded'
         >
           Next
