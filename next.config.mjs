bc3b2781a5c88d87feaf3dda0558dd502df2fd2c
agent-indigo <bradenhynes@gmail.com>
2024-08-30 20:41:21 -0230

    Cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-const VERCEL_URL = process.env.VERCEL_URL ?? ''
+const VERCEL_URL = process.env.VERCEL_URL
 const devUrl = 'http://localhost:3000'
 const prodUrl = `https://${VERCEL_URL}`
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_DOMAIN: VERCEL_URL === '' ? devUrl : prodUrl,
-    NEXT_PUBLIC_API_DOMAIN: `${VERCEL_URL === '' ? devUrl : prodUrl}/api`,
-    NEXTAUTH_URL: VERCEL_URL === '' ? devUrl : prodUrl
+    NEXT_PUBLIC_DOMAIN: VERCEL_URL ? prodUrl : devUrl,
+    NEXT_PUBLIC_API_DOMAIN: `${VERCEL_URL ? prodUrl : devUrl}/api`,
+    NEXTAUTH_URL: VERCEL_URL ? prodUrl : devUrl
   },
   images: {
     remotePatterns: [
