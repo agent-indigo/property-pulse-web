63930d405332569298064526e9aec61a20cd62b7
agent-indigo <bradenhynes@gmail.com>
2024-08-31 18:13:54 -0230

    Bug fix

--- serverActions/getSessionUser.ts
+++ serverActions/getSessionUser.ts
@@ -1,16 +1,28 @@
 'use server'
 import {getServerSession} from 'next-auth'
+import {FlattenMaps, ObjectId} from 'mongoose'
 import ServerActionResponse from '@/interfaces/ServerActionResponse'
 import SessionWithUserId from '@/interfaces/SessionWithUserId'
 import userModel from '@/models/userModel'
 import authOptions from '@/utilities/authOptions'
 import connectToMongoDB from '@/utilities/connectToMongoDB'
 import convertToPlainDocument from '@/utilities/convertToPlainDocument'
+import UserDocument from '@/interfaces/UserDocument'
+import PlainUser from '@/interfaces/PlainUser'
 const getSessionUser: Function = async (): Promise<ServerActionResponse> => {
   try {
     const session: SessionWithUserId | null = await getServerSession(authOptions)
     if (session) {
       await connectToMongoDB()
+      const user: FlattenMaps<UserDocument> | null = await userModel.findById(session.user.id).lean()
+      if (user) {
+        const sessionUser: PlainUser = convertToPlainDocument(user)
+        sessionUser.bookmarks = user.bookmarks.map((bookmark: ObjectId): string => bookmark.toString())
+        return {
+          sessionUser,
+          success: true
+        }
+      }
       return {
         sessionUser: convertToPlainDocument(await userModel.findById(session.user.id).lean()),
         success: true
