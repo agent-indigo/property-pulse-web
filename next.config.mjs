c1949716ea01cd28860ed18881de8ab51fdd19fb
agent-indigo <bradenhynes@gmail.com>
2025-01-04 18:20:25 -0330

    Added development-only NEXTAUTH_SECRET

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_DOMAIN: url,
     NEXT_PUBLIC_API_DOMAIN: `${url}/api`,
-    NEXTAUTH_URL: url
+    NEXTAUTH_URL: url,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? 'd3v3l0pm3nt53cr3tk3yn0t53cur3@t@11n3v3ru53!npr0duct!0n3v3r!!!'
   },
   images: {
     remotePatterns: [{
