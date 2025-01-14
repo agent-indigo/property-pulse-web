0f827174069b1db98ba75dffeb2bb905d1b217f7
agent-indigo <bradenhynes@gmail.com>
2025-01-14 13:52:45 -0330

    Restored deleted API route

---
+++ app/api/properties/user/[id]/route.ts
@@ -0,0 +1,34 @@
+import {
+  NextRequest,
+  NextResponse
+} from 'next/server'
+import propertyModel from '@/models/propertyModel'
+import userModel from '@/models/userModel'
+import connectToMongoDB from '@/utilities/connectToMongoDB'
+import dataResponse from '@/httpResponses/dataResponse'
+import notFoundResponse from '@/httpResponses/notFoundResponse'
+import serverErrorResponse from '@/httpResponses/serverErrorResponse'
+export const dynamic = 'force-dynamic'
+/**
+ * @name    GET
+ * @desc    Get all properties listed by the given user
+ * @route   GET /api/properties/user/:id
+ * @access  public
+ */
+export const GET = async (
+  request: NextRequest,
+  params: any
+): Promise<NextResponse> => {
+  try {
+    const {id}: any = params
+    await connectToMongoDB()
+    return await userModel.findById(id) ? dataResponse(await propertyModel.find({
+      owner: id
+    }).lean()) : notFoundResponse('User')
+  } catch (error: any) {
+    return serverErrorResponse(
+      'retrieving user\'s properties',
+      error
+    )
+  }
+}
