99fceb1b22f59300875721afb37a07c891994038
agent-indigo <bradenhynes@gmail.com>
2025-05-05 20:34:31 -0230

    Error 500 page improvements

--- app/error.tsx
+++ app/error.tsx
@@ -9,40 +9,52 @@ import ErrorPageProps from '@/types/ErrorPageProps'
 const ErrorPage: FunctionComponent<ErrorPageProps> = ({
   error,
   reset
-}): ReactElement => (
-  <section className='bg-blue-50 min-h-screen flex-grow'>
-    <div className='container m-auto max-w-2xl py-24'>
-      <div className='bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-        <div className='flex justify-center'>
-          <FaExclamationTriangle className='fa-5x text-8xl text-red-400'/>
-        </div>
-        <div className='text-center'>
-          <h1 className='text-3xl font-bold mt-4 mb02'>
-            500: Internal Server Error
-          </h1>
-          <h2 className='text-2xl font-bold my-2 text-red-400'>
-            {error.toString()}
-          </h2>
-          <div className='pb-1'>
-            <button
-              onClick={(): void => reset()}
-              className='bg-yellow-500 hover:bg-yellow-600 font-bold py-4 px-6 rounded'
-            >
-              Retry
-            </button>
+}): ReactElement => {
+  const {
+    message,
+    stack
+  }: Error = error
+  return (
+    <section className='bg-blue-50 min-h-screen flex-grow'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0'>
+          <div className='flex justify-center'>
+            <FaExclamationTriangle className='fa-5x text-8xl text-red-400'/>
           </div>
-          <div className='pt-1'>
-            <Link
-              href='/'
-              className='bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded inline-block'
-            >
-              Home
-            </Link>
+          <div className='text-center'>
+            <h1 className='text-3xl font-bold mt-4 mb-2'>
+              500: Internal Server Error
+            </h1>
+            <p className='text-2xl font-bold my-2 text-red-400'>
+              {message}
+            </p>
+            <h2 className="text-3xl font-bold mt-4 mb-2">
+              Stack
+            </h2>
+            <p className='text-2xl font-bold my-2 text-red-400'>
+              {stack}
+            </p>
+            <div className='pb-1'>
+              <button
+                onClick={(): void => reset()}
+                className='bg-yellow-500 hover:bg-yellow-600 font-bold py-4 px-6 rounded'
+              >
+                Retry
+              </button>
+            </div>
+            <div className='pt-1'>
+              <Link
+                href='/'
+                className='bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded inline-block'
+              >
+                Home
+              </Link>
+            </div>
           </div>
         </div>
+        <div className='flex-grow'/>
       </div>
-      <div className='flex-grow'/>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 export default ErrorPage
